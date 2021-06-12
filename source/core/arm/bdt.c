41735e8e96fd6e3c1924952c4715e83c056d206f
Arignir <github@sigint.sh>
2021-06-12 15:29:27 +0200

    Implement the S bit of block data transfers.

--- source/core/arm/bdt.c
+++ source/core/arm/bdt.c
@@ -21,18 +21,21 @@ core_arm_bdt(
     uint32_t base;
     uint32_t base_new;
     int32_t count;
-    bool reload_pipeline;
+    enum arm_modes mode_old;
+    bool mode_switch;
+    bool pc_in_rlist;
     bool first;
     bool load;
     bool pre;
+    bool s;
     bool wb;
 
     core = &gba->core;
-    reload_pipeline = false;
     rn = bitfield_get_range(op, 16, 20);
     load = bitfield_get(op, 20);
     pre = bitfield_get(op, 24);
     wb = bitfield_get(op, 21);
+    s = bitfield_get(op, 22);
 
     /*
     ** Count how many registers we are going to transfer
@@ -45,7 +48,17 @@ core_arm_bdt(
         ++i;
     }
 
+    /*
+    ** Edge case: if rlist is empty, transfer the pc but
+    ** increment the base as if all registers were transfered.
+    */
+    if (count == 0) {
+        op |= (1 << 15);
+        count = 16;
+    }
+
     base = core->registers[rn];
+    pc_in_rlist = bitfield_get(op, 15);
 
     /*
     ** Pre-calculate the end address and go incrementally from
@@ -62,6 +75,20 @@ core_arm_bdt(
         base_new = base;
     }
 
+    /*
+    ** User bank transfer:
+    **
+    ** The registers transferred are taken from the User bank rather
+    ** than the bank corresponding to the current mode
+    */
+    if (s && (!pc_in_rlist || !load)) {
+        mode_old = core->cpsr.mode;
+        core_switch_mode(core, MODE_USR);
+        mode_switch = true;
+    } else {
+        mode_switch = false;
+    }
+
     i = 0;
     first = true;
     while (i < 16) {
@@ -70,11 +97,8 @@ core_arm_bdt(
 
             if (load) {
                 core->registers[i] = mem_read32(gba, base);
-                printf("Loading [%08x] %08x to r%u\n", base, core->registers[i], i);
-                reload_pipeline |= (i == 15);
             } else {
                 mem_write32(gba, base, core->registers[i] + (i == 15) * 4);
-                printf("Storing r%u %08x to [%08x]\n", i, core->registers[i], base);
             }
 
             base += pre ? 0 : 4; // Post-increment
@@ -87,7 +111,18 @@ core_arm_bdt(
         ++i;
     }
 
-    if (reload_pipeline) {
+    if (pc_in_rlist && load) {
+        if (s) {
+            struct psr spsr;
+
+            spsr = core_spsr_get(core, core->cpsr.mode);
+            core_switch_mode(core, spsr.mode);
+            core->cpsr = spsr;
+        }
         core_reload_pipeline(gba);
+    }
+
+    if (mode_switch) { // Roll back to previous mode
+        core_switch_mode(core, mode_old);
     }
 }
