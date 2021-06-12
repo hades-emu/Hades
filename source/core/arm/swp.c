513fb0029435faceeafafa3192d98a5e29ff69c8
Arignir <github@sigint.sh>
2021-06-12 02:25:14 +0200

    Implement the Single Data Swap (SWP) instruction.

---
+++ source/core/arm/swp.c
@@ -0,0 +1,41 @@
+/******************************************************************************\
+**
+**  This file is part of the Hades GBA Emulator, and is made available under
+**  the terms of the GNU General Public License version 2.
+**
+**  Copyright (C) 2021 - The Hades Authors
+**
+\******************************************************************************/
+
+#include "hades.h"
+#include "gba.h"
+
+/*
+** Execute the Single Data Swap instruction.
+*/
+void
+core_arm_swp(
+    struct gba *gba,
+    uint32_t op
+) {
+    struct core *core;
+    uint32_t tmp;
+    uint32_t rm;
+    uint32_t rn;
+    uint32_t rd;
+
+    core = &gba->core;
+    rm = bitfield_get_range(op, 0, 4);
+    rd = bitfield_get_range(op, 12, 16);
+    rn = bitfield_get_range(op, 16, 20);
+
+    if (bitfield_get(op, 22)) { // Swap byte quantity
+        tmp = mem_read8(gba, core->registers[rn]);
+        mem_write8(gba, core->registers[rn], core->registers[rm]);
+        core->registers[rd] = tmp;
+    } else { // Swap word quantity
+        tmp = mem_read32(gba, core->registers[rn]);
+        mem_write32(gba, core->registers[rn], core->registers[rm]);
+        core->registers[rd] = tmp;
+    }
+}
