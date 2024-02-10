b643f1423fcff70d21ae370376bcd1b02b9de087
Arignir <github@sigint.sh>
2024-02-11 00:23:30 +0100

    Update the copyright notice for 2024.

--- include/app/lang.h
+++ include/app/lang.h
@@ -4,7 +4,7 @@
 **  This file is part of the Hades GBA Emulator, and is made available under
 **  the terms of the GNU General Public License version 2.
 **
-**  Copyright (C) 2021-2023 - The Hades Authors
+**  Copyright (C) 2021-2024 - The Hades Authors
 **
 \******************************************************************************/
 
