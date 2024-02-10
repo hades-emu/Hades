1aa1946f1d166b7ef9b6547e4240ad7b91ece570
Arignir <github@sigint.sh>
2024-02-10 23:30:21 +0100

    Use the system directories for the configuration and the screenshots on Linux.

--- source/app/path.c
+++ source/app/path.c
@@ -11,6 +11,85 @@
 #include "app/app.h"
 #include "compat.h"
 
+/*
+** Return the platform-dependent configuration directory or NULL
+** if the system doesn't have one.
+**
+** The returned value must be freed.
+*/
+static inline
+char *
+system_config_dir(void)
+{
+#if __APPLE__
+    char *home_dir;
+
+    home_dir = getenv("HOME");
+    if (home_dir && hs_fexists(home_dir)) {
+        return (hs_format("%s/Library/Application Support", home_dir));
+    }
+
+    return (NULL);
+#elif __unix__
+    char *xdg_config_dir;
+    char *home_dir;
+
+    xdg_config_dir = getenv("XDG_CONFIG_HOME");
+    if (xdg_config_dir && hs_fexists(xdg_config_dir)) {
+        return (strdup(xdg_config_dir));
+    }
+
+    home_dir = getenv("HOME");
+    if (home_dir && hs_fexists(home_dir)) {
+        return (hs_format("%s/.config", home_dir));
+    }
+
+    return (NULL);
+#else
+    return (NULL);
+#endif
+}
+
+/*
+** Return the platform-dependent pictures directory or NULL
+** if the system doesn't have one.
+**
+** The returned value must be freed.
+*/
+static inline
+char *
+system_pictures_dir(void)
+{
+#if __APPLE__
+    char *home_dir;
+
+    home_dir = getenv("HOME");
+    if (home_dir && hs_fexists(home_dir)) {
+        return (hs_format("%s/Pictures", home_dir));
+    }
+
+    return (NULL);
+#elif __unix__
+    char *xdg_pictures_dir;
+    char *home_dir;
+
+    xdg_pictures_dir = getenv("XDG_PICTURES_DIR");
+    if (xdg_pictures_dir && hs_fexists(xdg_pictures_dir)) {
+        return (strdup(xdg_pictures_dir));
+    }
+
+    home_dir = getenv("HOME");
+    if (home_dir && hs_fexists(home_dir)) {
+        return (strdup(home_dir));
+    }
+
+    return (NULL);
+#else
+    return (NULL);
+#endif
+}
+
+
 void
 app_paths_update(
     struct app *app
@@ -18,8 +97,8 @@ app_paths_update(
     char *sys_config_dir;
     char *sys_pictures_dir;
 
-    sys_config_dir = hs_system_config_dir();
-    sys_pictures_dir = hs_system_pictures_dir();
+    sys_config_dir = system_config_dir();
+    sys_pictures_dir = system_pictures_dir();
 
     if (sys_config_dir && hs_fexists(sys_config_dir)) {
         char *hades_config_dir;
